d5c2dba0e6e2122ba83ce590c3f448ab1a3164d4
hitoshi <g.hitoshi.yokogawa@gmail.com>
2022-05-19 19:26:29 -0300

    Projeto Sampers criado

---
+++ sampers-api/src/main/java/com/samperswork/sampers/SampersApiApplication.java
@@ -0,0 +1,13 @@
+package com.samperswork.sampers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampersApiApplication.class, args);
+	}
+
+}
